e8e4f41363e11e47d3206465fdb00ad1c797f671
bronicles <72601426+bronicles@users.noreply.github.com>
2022-02-18 16:43:02 +0100

    fetch with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import User from "./components/user";
 import Home from "./pages/home";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/header";
--- src/pages/user-statistics.jsx
+++ src/pages/user-statistics.jsx
@@ -1,11 +1,52 @@
 import '../styles/user-statistics.css'
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function UserStatistics() {
-
 	const {id} = useParams()
-	console.log(id)
+	const [data, setData] = useState({})
+	const [activity, setActivity] = useState({})
+	const [sessions, setSessions] = useState({})
+	const [performance, setPerformance] = useState({})
 
+	useEffect( () => {
+		fetch(`http://localhost:3000/user/${id}`)
+			.then(res => res.json())
+			.then(user => {
+				setData(user)
+			})
+	}, [] )
+	console.log('user data', data)
+
+	useEffect( () => {
+		fetch(`http://localhost:3000/user/${id}/activity`)
+			.then(res => res.json())
+			.then(user => {
+				setActivity(user)
+			})
+		}, []
+	)
+	console.log('activity', activity)
+
+	useEffect( () => {
+		fetch(`http://localhost:3000/user/${id}/average-sessions`)
+			.then(res => res.json())
+			.then(user => {
+				setSessions(user)
+
+			})
+		}, []
+	)
+	console.log('average sessions', sessions)
+
+	useEffect(() => {
+		fetch(`http://localhost:3000/user/${id}/performance`)
+			.then(res => res.json())
+			.then(user => {
+				setPerformance(user)
+			})
+	},[] )
+	console.log('performance', performance)
 
 	return <div>
 		user statistics {id}
